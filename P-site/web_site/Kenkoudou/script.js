c82a653e8c87d7fcdac56036f2daada12632f0cb
suzuki0909 <95904254+suzuki0909@users.noreply.github.com>
2021-12-28 21:04:22 +0900

    12/28最初のアップ

---
+++ P-site/web_site/Kenkoudou/script.js
@@ -0,0 +1,18 @@
+$('.slider').slick({
+    fade:true,//切り替えをフェードで行う。初期値はfalse。
+    autoplay: true,//自動的に動き出すか。初期値はfalse。
+    autoplaySpeed: 1500,//次のスライドに切り替わる待ち時間
+    speed:3000,//スライドの動きのスピード。初期値は300。
+    slidesToShow: 1,//スライドを画面に3枚見せる
+    slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+    arrows: true,//左右の矢印あり
+    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+    pauseOnFocus: false,//フォーカスで一時停止を無効
+    pauseOnHover: false,//マウスホバーで一時停止を無効
+});
+
+//スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
+$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
+$('.slider').slick('slickPlay');
+});
